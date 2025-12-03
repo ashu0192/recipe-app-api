d2336ce84605485781f05055835b8e065ae91d32
Ashutosh Tripathi <ashutoshtripathi@Ashutoshs-MacBook-Air.local>
2025-12-03 12:21:54 +0530

    checking  process

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,10 +6,8 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
-    """Test models."""
 
     def test_create_user_with_email_successful(self):
-        """Test creating a user with an email is successful."""
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
@@ -22,7 +20,6 @@ class ModelTests(TestCase):
 
 
     def test_create_recipe(self):
-        """test creating a recipie is susseccfull """
 
         user = get_user_model().objects.create_user(
             'test@example.com',
